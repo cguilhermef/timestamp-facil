d025e277584f712f131123f40b195463aa26a0a0
Christian Guilherme Fritsch <cguilhermef@users.noreply.github.com>
2022-05-25 18:48:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+let timer = null;
 document.addEventListener('DOMContentLoaded', function () {
+  const b = new ClipboardJS('#btn-copy');
   const t = document.getElementsByClassName('js-timestamp')[0];
   t.innerHTML = new Date()
     .toISOString()
     .replace(/\:/g, '')
     .replace(/\-/g, '')
     .split('.')[0];
+
+  clearTimeout(timer);
+
+  b.on('success', function () {
+    const c = document.querySelector('.copied');
+    c.style.display = 'flex';
+    timer = setTimeout(function () {
+      c.style.display = 'none';
+    }, 1500);
+  });
 });
+
+function generate() {
+  const t = document.getElementsByClassName('js-timestamp')[0];
+  const stamp = new Date()
+    .toISOString()
+    .replace(/\:/g, '')
+    .replace(/\-/g, '')
+    .split('.')[0];
+  t.innerHTML = stamp;
+  document
+    .getElementById('btn-copy')
+    .setAttribute('data-clipboard-text', stamp);
+}
+
+setInterval(function () {
+  generate();
+}, 5000);
